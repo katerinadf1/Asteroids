1c947ffbc9b118924133dfc14efe558f1266a668
Zeldork93 <katerinadf1@gmail.com>
2025-05-18 10:47:35 -0700

    Spinning around at the speed of sound

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import pygame
-
 from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     game_clock = pygame.time.Clock()
     dt = 0
 
+    player1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player1.update(dt)
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
-        game_clock.tick(60)
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
